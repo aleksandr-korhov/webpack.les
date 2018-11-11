4ec318910f9257a0d800faa01dac824502a0092c
Aleksandr <akorhov@usetech.ru>
2018-11-12 00:36:01 +0300

    tree shaking lesson

--- src/index.js
+++ src/index.js
@@ -1,18 +1,36 @@
-import _ from "lodash";
-import printMe from "./print";
+// import _ from "lodash";
+// import printMe from "./print";
+// import "./style.css";
+import { cube } from "./math";
 
 function component() {
-    let element = document.createElement('div');
-    let btn = document.createElement('button');
-  
-    // Lodash, currently included via a script, is required for this line to work
-    element.innerHTML = _.join(['Hello', 'webpack','!'], ' ');
+  // let element = document.createElement('div');
+  let element = document.createElement('pre');
+  let btn = document.createElement('button');
 
-    btn.innerHTML = 'Click me and check the console!';
-    btn.onclick = printMe;
-    element.appendChild(btn);
-  
-    return element;
-  }
-  
-  document.body.appendChild(component());
+  // Lodash, currently included via a script, is required for this line to work
+  // element.innerHTML = _.join(['Hello', 'webpack', '!'], ' ');
+  element.innerHTML = [
+    'Hello webpack',
+    '5 cubed is equal to ' + cube(5)
+  ].join('\n\n');
+
+  // btn.innerHTML = 'Click me and check the console!';
+  // btn.onclick = printMe;
+  // element.appendChild(btn);
+
+  return element;
+}
+
+let element = component();
+document.body.appendChild(element);
+
+// if (module.hot) {
+//   module.hot.accept('./print.js', function () {
+//     console.log('Accepting the updated printMe module!');
+
+//     document.removeChild(element);
+//     element = component();
+//     document.appendChild(element);
+//   })
+// }
--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,36 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
+const webpack = require('webpack');
 
 module.exports = {
+  mode: "production",
+  devtool: "inline-source-map",
+  devServer: {
+    contentBase: './dist',
+    hot: true
+  },
   entry: {
-    app: './src/index.js',
-    print: './src/print.js'
+    app: './src/index.js'
   },
   plugins: [
     new CleanWebpackPlugin(['dist']),
     new HtmlWebpackPlugin({
-      title: 'Output Management'
-    })
+      title: 'Hot module replacement'
+    }),
+    new webpack.HotModuleReplacementPlugin()
   ],
   output: {
     filename: '[name].bundle.js',
-    path: path.resolve(__dirname, 'dist')
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '/'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      }
+    ]
   }
 };
